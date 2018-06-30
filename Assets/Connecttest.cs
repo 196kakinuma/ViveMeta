ccee45ff96aa5c1297353a2d6d10289cd35c3450
196kakinuma <196thinline@gmail.com>
2018-06-30 18:03:18 +0900

    調整

---
+++ Assets/Connecttest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Connecttest : MonoBehaviour
+{
+
+    public GameObject cube;
+    // Use this for initialization
+    void Start ()
+    {
+        NetworkServer.Spawn (Instantiate (cube));
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
