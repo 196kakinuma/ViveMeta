b6ca9e75941a781371f31864e6f0fd567af12f84
196kakinuma <196thinline@gmail.com>
2018-06-30 17:09:34 +0900

    InitializeSettingでViveの動作確認

---
+++ Assets/Scripts/System/InitializeSettings.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ViveMeta.System
+{
+    enum DeviceType
+    {
+        VIVE,
+        META,
+        HOLO
+    }
+
+
+
+    public class InitializeSettings : MonoBehaviour
+    {
+        [SerializeField]
+        DeviceType deviceType = DeviceType.VIVE;
+
+        [SerializeField]
+        GameObject ViveCameras;
+        [SerializeField]
+        GameObject MetaCameras;
+
+
+
+        void Awake ()
+        {
+            if ( deviceType == DeviceType.VIVE )
+            {
+                ViveCameras.SetActive (true);
+            }
+            else if ( deviceType == DeviceType.META )
+            {
+                MetaCameras.SetActive (true);
+            }
+            else
+            {
+                Debug.LogError ("No DeviceType Selected");
+            }
+        }
+
+
+        // Use this for initialization
+        void Start ()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+    }
+}
